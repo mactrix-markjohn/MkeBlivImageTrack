9e82f263587e9e280df60dd67d65969ff4c372e1
mactrix-markjohn <chukwuemekaokparaeke@gmail.com>
2023-08-19 22:26:06 +0100

    Script changes

--- Assets/Scripts/ButtonClickScript.cs
+++ Assets/Scripts/ButtonClickScript.cs
@@ -20,8 +20,8 @@ public class ButtonClickScript : MonoBehaviour
     public void BackToMenu()
     {
         //SceneManager.LoadScene(StringStore.MenuScene);
-        SceneManager.LoadScene("TestCaptureVideoARLensScene");
-        //Application.Quit();
+        //SceneManager.LoadScene("TestCaptureVideoARLensScene");
+        Application.Quit();
     }
     
     
